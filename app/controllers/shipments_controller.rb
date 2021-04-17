33b0830573fef552b06a05642491ae901764fa6a
Your Name <you@example.com>
2021-04-17 20:39:03 +0530

    Added parameter

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -10,7 +10,7 @@ class ShipmentsController < ApplicationController
     puts(origin)
     @routes = Route.includes(:origin, :destination, :trucks)         
       .where(origin: origin, destination: destination)
-      .select("trucks.name,trucks.capacity_in_kg,truck_routes.start_date,routes.origin_id,routes.destination_id") 
+      .select("trucks.name,trucks.capacity_in_kg,truck_routes.start_date,truck_routes.id as truck_route_id,routes.origin_id,routes.destination_id") 
       .joins(:truck_routes).where(truck_routes: {start_date: start_date..(start_date + 365.days) } )
       .joins(:trucks).where("trucks.id = truck_routes.truck_id and trucks.capacity_in_kg >?",capacity_in_kg)
       puts(@routes)
