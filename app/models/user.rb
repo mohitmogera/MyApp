120bcc416cab3de7351cda0f49b1d184bfb8ec27
Your Name <you@example.com>
2021-04-18 10:39:13 +0530

    commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable,:trackable, :validatable
 
          def generate_jwt
+          puts("generate jwt key")
+          puts(Rails.application.secret_key_base)
           JWT.encode({ id: id,
                       exp: 60.days.from_now.to_i },
-                     Rails.application.secrets.secret_key_base)
+                     Rails.application.secret_key_base)
         end
 end
